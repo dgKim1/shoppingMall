533c88dc5dbffc8dd84638634cf729bde30b37e1
dgKim1 <kimdg3556@naver.com>
2026-01-13 06:24:38 +0900

    feat: 구글 oauth 로그인 기능 추가

--- routes/auth.api.js
+++ routes/auth.api.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const authController = require("../controllers/auth.controller")
+const userController = require("../controllers/user.controller")
 
 //회원가입
 router.post("/login", authController.loginWithEmail);
+router.post("/google", userController.loginWithGoogle);
 
 module.exports= router;
