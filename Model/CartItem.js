43a0a1c32714816b0b91ac1b939a5a67cb4b00e3
dgKim1 <kimdg3556@naver.com>
2026-01-18 11:15:59 +0900

    feat: cart에 item추가시 color 정보도 요구하는 방식으로 수정

--- Model/CartItem.js
+++ Model/CartItem.js
@@ -5,6 +5,7 @@ const cartItemSchema = new Schema({
   cartId: { type: Schema.Types.ObjectId, ref: "Cart", required: true },
   productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
   size: { type: String, required: true },
+  color: { type: String, required: true },
   quantity: { type: Number, required: true, default: 1 },
 });
 
